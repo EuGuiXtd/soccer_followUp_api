260ad19906efe1d87c263b321325194606e3000e
EuGuiXtd <guilhermedpjf@gmail.com>
2023-02-28 18:48:45 -0300

    fix: arrumando avaliador do git

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,53 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  role: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  email: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(255),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
